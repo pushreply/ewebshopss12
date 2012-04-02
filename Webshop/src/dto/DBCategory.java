b1b4963311b9c6fc967eb85e9d1c6b69e94fffa8
ronaldo.hasiholan@gmail.com <ronaldo.hasiholan@gmail.com@323ef242-e3e8-b9c7-4c3b-e6e2eb09e930>
2012-04-02 10:28:04 +0000

    DTO v.1

---
+++ Webshop/src/dto/DBCategory.java
@@ -0,0 +1,34 @@
+/**
+ * 
+ */
+package dto;
+import com.db4o.*;
+
+/**
+ * @author roha0001
+ *
+ */
+public class DBCategory {
+	private String categoryName;
+
+	public DBCategory(String categoryName) {
+		super();
+		this.categoryName = categoryName;
+	}
+
+	public String getCategoryName() {
+		return categoryName;
+	}
+
+	public void setCategoryName(String categoryName) {
+		this.categoryName = categoryName;
+	}
+
+	@Override
+	public String toString() {
+		return "DBCategory [categoryName=" + categoryName + "]";
+	}
+	
+	
+
+}
---
+++ Webshop/src/dto/DBKeyword.java
@@ -0,0 +1,34 @@
+/**
+ * 
+ */
+package dto;
+import com.db4o.*;
+
+/**
+ * @author roha0001
+ *
+ */
+public class DBKeyword {
+	private String keywordName;
+
+	public DBKeyword(String keywordName) {
+		super();
+		this.keywordName = keywordName;
+	}
+
+	public String getKeywordName() {
+		return keywordName;
+	}
+
+	public void setKeywordName(String keywordName) {
+		this.keywordName = keywordName;
+	}
+
+	@Override
+	public String toString() {
+		return "DBKeyword [keywordName=" + keywordName + "]";
+	}
+	
+	
+
+}
