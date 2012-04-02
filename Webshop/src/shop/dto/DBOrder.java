deb55600bf4f9c138c4f302eae91021bf741bbfd
sesc0009@gmail.com <sesc0009@gmail.com@323ef242-e3e8-b9c7-4c3b-e6e2eb09e930>
2012-04-02 22:31:54 +0000

    anderung von paket

---
+++ Webshop/src/shop/dto/DBOrder.java
@@ -0,0 +1,58 @@
+package shop.dto;
+import java.util.Date;
+
+
+/**
+ * @author roha0001
+ *
+ */
+
+public class DBOrder {
+
+	public class Order {
+		
+		private DBCustomer cust;
+		private Date datum;
+		private DBItems item;
+		private int state;
+		
+		public Order(DBCustomer cust, Date datum, DBItems item, int state) {
+			super();
+			this.cust = cust;
+			this.datum = datum;
+			this.item = item;
+			this.state = state;
+		}
+		public DBCustomer getCust() {
+			return cust;
+		}
+		public void setCust(DBCustomer cust) {
+			this.cust = cust;
+		}
+		public Date getDatum() {
+			return datum;
+		}
+		public void setDatum(Date datum) {
+			this.datum = datum;
+		}
+		public DBItems getItem() {
+			return item;
+		}
+		public void setItem(DBItems item) {
+			this.item = item;
+		}
+		public int getState() {
+			return state;
+		}
+		public void setState(int state) {
+			this.state = state;
+		}
+		@Override
+		//maybe useful for logging.
+		public String toString() {
+			return "Order [cust=" + cust + ", datum=" + datum + ", item="
+					+ item + "]";
+		}
+	}
+		
+}
