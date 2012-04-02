01515f683424809acdf07d0f7a46a0db84124043
ronaldo.hasiholan@gmail.com <ronaldo.hasiholan@gmail.com@323ef242-e3e8-b9c7-4c3b-e6e2eb09e930>
2012-04-02 10:28:04 +0000

    DTO v.1

---
+++ trunk/Webshop/src/dto/DBCustomer.java
@@ -0,0 +1,54 @@
+package dto;
+import com.db4o.*;
+/**
+ * @author roha0001
+ *
+ */
+public class DBCustomer {
+	private String login; 
+	private String password; 
+	private DBAddress mailadress;
+	private String nickname;
+	
+	public DBCustomer(String login, String password, DBAddress mailadress,
+			String nickname) {
+		super();
+		this.login = login;
+		this.password = password;
+		this.mailadress = mailadress;
+		this.nickname = nickname;
+	}
+	public String getLogin() {
+		return login;
+	}
+	public void setLogin(String login) {
+		this.login = login;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public DBAddress getMailadress() {
+		return mailadress;
+	}
+	public void setMailadress(DBAddress mailadress) {
+		this.mailadress = mailadress;
+	}
+	public String getNickname() {
+		return nickname;
+	}
+	public void setNickname(String nickname) {
+		this.nickname = nickname;
+	}
+	@Override
+	//maybe useful for logging.
+	public String toString() {
+		return "Customer [login=" + login + ", password=" + password
+				+ ", mailadress=" + mailadress + ", nickname=" + nickname + "]";
+	} 
+	
+	
+
+}
---
+++ trunk/Webshop/src/dto/DBItems.java
@@ -0,0 +1,35 @@
+/**
+ * 
+ */
+package dto;
+import com.db4o.*;
+
+/**
+ * @author roha0001
+ *
+ */
+public class DBItems {
+	private int orderAmount;
+
+	public DBItems(int orderAmount) {
+		super();
+		this.orderAmount = orderAmount;
+	}
+
+	public int getOrderAmount() {
+		return orderAmount;
+	}
+
+	public void setOrderAmount(int orderAmount) {
+		this.orderAmount = orderAmount;
+	}
+
+	@Override
+	//maybe useful for logging.
+	public String toString() {
+		return "Items [orderAmount=" + orderAmount + "]";
+	}
+	
+	
+
+}
