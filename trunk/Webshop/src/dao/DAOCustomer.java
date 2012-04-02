d342143ffe00a360d6aa8060a80814d2a1d8e029
mukujapau@gmail.com <mukujapau@gmail.com@323ef242-e3e8-b9c7-4c3b-e6e2eb09e930>
2012-04-02 21:22:55 +0000

    DAOCustomer als Beispiel mit Methoden versehen

--- trunk/Webshop/src/dao/DAOCustomer.java
+++ trunk/Webshop/src/dao/DAOCustomer.java
@@ -1,5 +1,30 @@
 package dao;
 
-public class DAOCustomer {
+import java.util.List;
+import com.db4o.ObjectSet;
+import com.db4o.ObjectContainer;
+import dto.DBCustomer;
 
+public class DAOCustomer {
+	
+	public static ObjectSet<DBCustomer> findAllCustomers(ObjectContainer db)
+	{
+	   return null;	
+	}
+	
+	public static DBCustomer findCustomerByUsername(ObjectContainer db, String login)
+	{
+		
+		return null;
+	}
+	
+	public static void UpdateCustomers(ObjectContainer db)
+	{
+		
+	}
+    
+	public static void DeleteCustommer(ObjectContainer db)
+	{
+		
+	}
 }
