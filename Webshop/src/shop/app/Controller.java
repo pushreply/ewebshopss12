12029682e0cf6321aef37021b0147ceb2617ddbc
benjamin.oswald1990@gmail.com <benjamin.oswald1990@gmail.com@323ef242-e3e8-b9c7-4c3b-e6e2eb09e930>
2012-04-03 12:53:25 +0000

    Änderung Controller - Benjamin

--- Webshop/src/shop/app/Controller.java
+++ Webshop/src/shop/app/Controller.java
@@ -2,8 +2,10 @@
  * @author Benjamin
  *
  */
- 
+
 package shop.app;
+
+import javax.servlet.http.*;
 
 import com.db4o.Db4oEmbedded;
 import com.db4o.ObjectContainer;
@@ -18,54 +20,90 @@ import shop.dto.DBKeyword;
 import shop.dto.DBOrder;
 import shop.dto.DBTrack;
 
-public class Controller {
+public class Controller extends HttpServlet {
 
-public static void main(String[]args){			// main-Methode Anfang
+	public void service(HttpServletRequest request, HttpServletResponse response){			// service-Methode Anfang
+
+		System.out.println("Test");
+		
+		/*ObjectContainer db = null;
 	
-	// String file = ""							//Dateiname der DB4O-Datenbank
+		EmbeddedConfiguration config = Db4oEmbedded.newConfiguration();
 	
-	EmbeddedConfiguration config = Db4oEmbedded.newConfiguration();
+		String file = "WebshopDB.dbf";
+		
 	
-	ObjectContainer con = null;
+		//Klasse Address
+		config.common().objectClass(DBAddress.class).cascadeOnUpdate(true);
+		config.common().objectClass(DBAddress.class).cascadeOnDelete(true);
+		config.common().objectClass(DBAddress.class).objectField("street").indexed(true);
+		config.common().objectClass(DBAddress.class).objectField("country").indexed(true);
+		config.common().objectClass(DBAddress.class).objectField("firstName").indexed(true);
+		config.common().objectClass(DBAddress.class).objectField("lastName").indexed(true);
+		config.common().objectClass(DBAddress.class).objectField("gender").indexed(true);
 	
-	//Klasse Address
-	config.common().objectClass(DBAddress.class).cascadeOnUpdate(true);
-	config.common().objectClass(DBAddress.class).cascadeOnDelete(true);
-	config.common().objectClass(DBAddress.class).objectField("street").indexed(true);
-	config.common().objectClass(DBAddress.class).objectField("country").indexed(true);
-	config.common().objectClass(DBAddress.class).objectField("firstName").indexed(true);
-	config.common().objectClass(DBAddress.class).objectField("lastName").indexed(true);
-	config.common().objectClass(DBAddress.class).objectField("gender").indexed(true);
-	
-	//Klasse Album
-	config.common().objectClass(DBAlbum.class).cascadeOnUpdate(true);
-	config.common().objectClass(DBAlbum.class).cascadeOnDelete(true);
-	
-	
-	//Klasse Category
-	config.common().objectClass(DBCategory.class).cascadeOnUpdate(true);
-	config.common().objectClass(DBCategory.class).cascadeOnDelete(true);
-	
-	//Klasse Customer
-	config.common().objectClass(DBCustomer.class).cascadeOnUpdate(true);
-	config.common().objectClass(DBCustomer.class).cascadeOnDelete(true);
-	
-	//Klasse Items
-	config.common().objectClass(DBItems.class).cascadeOnUpdate(true);
-	config.common().objectClass(DBItems.class).cascadeOnDelete(true);
-	
-	//Klasse Keyword
-	config.common().objectClass(DBKeyword.class).cascadeOnUpdate(true);
-	config.common().objectClass(DBKeyword.class).cascadeOnDelete(true);
-	
-	//Klasse Order
-	config.common().objectClass(DBOrder.class).cascadeOnUpdate(true);
-	config.common().objectClass(DBOrder.class).cascadeOnDelete(true);
-	
-	//Klasse Track
-	config.common().objectClass(DBTrack.class).cascadeOnUpdate(true);
-	config.common().objectClass(DBTrack.class).cascadeOnDelete(true);
-	
-}	// Ende der main-Methode
-	
+		//Klasse Album
+		config.common().objectClass(DBAlbum.class).cascadeOnUpdate(true);
+		config.common().objectClass(DBAlbum.class).cascadeOnDelete(true);
+		config.common().objectClass(DBAlbum.class).objectField("coverpath").indexed(true);
+		config.common().objectClass(DBAlbum.class).objectField("albumTitel").indexed(true);
+		config.common().objectClass(DBAlbum.class).objectField("artist").indexed(true);
+		config.common().objectClass(DBAlbum.class).objectField("numberOfDisks").indexed(true);
+		config.common().objectClass(DBAlbum.class).objectField("price").indexed(true);
+		config.common().objectClass(DBAlbum.class).objectField("amount").indexed(true);
+		config.common().objectClass(DBAlbum.class).objectField("numberOfTracks").indexed(true);
+		config.common().objectClass(DBAlbum.class).objectField("label").indexed(true);
+		
+		//Klasse Category
+		config.common().objectClass(DBCategory.class).cascadeOnUpdate(true);
+		config.common().objectClass(DBCategory.class).cascadeOnDelete(true);
+		config.common().objectClass(DBCategory.class).objectField("categoryName").indexed(true);
+		
+		//Klasse Customer
+		config.common().objectClass(DBCustomer.class).cascadeOnUpdate(true);
+		config.common().objectClass(DBCustomer.class).cascadeOnDelete(true);
+		config.common().objectClass(DBCustomer.class).objectField("login").indexed(true);
+		config.common().objectClass(DBCustomer.class).objectField("password").indexed(true);
+		config.common().objectClass(DBCustomer.class).objectField("mailadress").indexed(true);
+		config.common().objectClass(DBCustomer.class).objectField("nickname").indexed(true);
+		
+		//Klasse Items
+		config.common().objectClass(DBItems.class).cascadeOnUpdate(true);
+		config.common().objectClass(DBItems.class).cascadeOnDelete(true);
+		config.common().objectClass(DBItems.class).objectField("orderAmount").indexed(true);
+		
+		//Klasse Keyword
+		config.common().objectClass(DBKeyword.class).cascadeOnUpdate(true);
+		config.common().objectClass(DBKeyword.class).cascadeOnDelete(true);
+		config.common().objectClass(DBKeyword.class).objectField("keywordName").indexed(true);
+		
+		//Klasse Order
+		config.common().objectClass(DBOrder.class).cascadeOnUpdate(true);
+		config.common().objectClass(DBOrder.class).cascadeOnDelete(true);
+		config.common().objectClass(DBOrder.class).objectField("cust").indexed(true);
+		config.common().objectClass(DBOrder.class).objectField("datum").indexed(true);
+		config.common().objectClass(DBOrder.class).objectField("item").indexed(true);
+		config.common().objectClass(DBOrder.class).objectField("state").indexed(true);
+		
+		//Klasse Track
+		config.common().objectClass(DBTrack.class).cascadeOnUpdate(true);
+		config.common().objectClass(DBTrack.class).cascadeOnDelete(true);
+		config.common().objectClass(DBOrder.class).objectField("file").indexed(true);
+		config.common().objectClass(DBOrder.class).objectField("trackTitle").indexed(true);
+		config.common().objectClass(DBOrder.class).objectField("trackArtist").indexed(true);
+		config.common().objectClass(DBOrder.class).objectField("trackDate").indexed(true);
+		config.common().objectClass(DBOrder.class).objectField("trackGenre").indexed(true);
+		config.common().objectClass(DBOrder.class).objectField("trackNumber").indexed(true);
+		config.common().objectClass(DBOrder.class).objectField("trackDiskNumber").indexed(true);
+		
+		db = Db4oEmbedded.openFile(config, file);
+		
+		/*DBKeyword test = new DBKeyword("Michael Schumacher");
+		db.store(test);
+		System.out.println("Stored " + test);*/
+		
+		//db.close();
+		
+	}	// Ende der service-Methode
+
 }	// Ende Klasse Controller
