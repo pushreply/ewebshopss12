a07e5b7444358ae08f35bb4fe3d5e04d400093ab
ronaldo.hasiholan@gmail.com <ronaldo.hasiholan@gmail.com@323ef242-e3e8-b9c7-4c3b-e6e2eb09e930>
2012-04-03 11:32:31 +0000

    customer-address Beziehung

--- Webshop/src/shop/dto/DBCustomer.java
+++ Webshop/src/shop/dto/DBCustomer.java
@@ -1,4 +1,7 @@
 package shop.dto;
+
+import java.util.LinkedList;
+
 /**
  * @author roha0001
  *
@@ -6,46 +9,57 @@ package shop.dto;
 public class DBCustomer {
 	private String login; 
 	private String password; 
-	private DBAddress mailadress;
 	private String nickname;
 	
-	public DBCustomer(String login, String password, DBAddress mailadress,
+	//A Customer has one or more addresses
+	LinkedList<DBAddress> address = new LinkedList<DBAddress>();
+
+	public LinkedList<DBAddress> getAddress() {
+		return address;
+	}
+	
+	public void setAddress(LinkedList<DBAddress> address) {
+		this.address = address;
+	}
+	
+	public DBCustomer(String login, String password, LinkedList<DBAddress> address,
 			String nickname) {
 		super();
 		this.login = login;
 		this.password = password;
-		this.mailadress = mailadress;
+		this.address = address;
 		this.nickname = nickname;
 	}
+	
 	public String getLogin() {
 		return login;
 	}
+	
 	public void setLogin(String login) {
 		this.login = login;
 	}
+	
 	public String getPassword() {
 		return password;
 	}
+	
 	public void setPassword(String password) {
 		this.password = password;
 	}
-	public DBAddress getMailadress() {
-		return mailadress;
-	}
-	public void setMailadress(DBAddress mailadress) {
-		this.mailadress = mailadress;
-	}
+
 	public String getNickname() {
 		return nickname;
 	}
+	
 	public void setNickname(String nickname) {
 		this.nickname = nickname;
 	}
+	
 	@Override
 	//maybe useful for logging.
 	public String toString() {
 		return "Customer [login=" + login + ", password=" + password
-				+ ", mailadress=" + mailadress + ", nickname=" + nickname + "]";
+				+ ", mailadress=" + address + ", nickname=" + nickname + "]";
 	} 
 	
 	
