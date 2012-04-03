35f7100a3aa8c2b4446a35b9b735aefbf0ebfa22
sesc0009@gmail.com <sesc0009@gmail.com@323ef242-e3e8-b9c7-4c3b-e6e2eb09e930>
2012-04-03 09:43:09 +0000

    ByteArray

---
+++ trunk/Webshop/src/shop/util/ByteArray.java
@@ -0,0 +1,38 @@
+package shop.util;
+
+import java.io.ByteArrayOutputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
+public class ByteArray {
+	
+	static public byte[] inputStreamToByteArray(InputStream inStream) throws IOException { 
+	    ByteArrayOutputStream baos = new ByteArrayOutputStream(); 
+	    byte[] buffer = new byte[8192]; 
+	    int bytesRead; 
+	    while ((bytesRead = inStream.read(buffer)) > 0) { 
+	        baos.write(buffer, 0, bytesRead); 
+	    } 
+	    return baos.toByteArray(); 
+	} 
+
+	
+	
+	static public void byteArrayToFile(byte[] byteArray, String outFilePath) throws IOException
+	{
+		FileOutputStream fos;
+		try {
+			fos = new FileOutputStream(outFilePath);
+			fos.write(byteArray);
+			fos.close();
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		
+	} 
+
+}
