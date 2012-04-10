53c03382e272950a097a0e3999fc995812872a75
andanima@gmail.com <andanima@gmail.com@323ef242-e3e8-b9c7-4c3b-e6e2eb09e930>
2012-04-10 16:07:07 +0000

    improved Trackfaktory (added test) and fixed date in DTO & DAO class

--- trunk/Webshop/src/shop/util/TrackfactoryTest.java
+++ trunk/Webshop/src/shop/util/TrackfactoryTest.java
@@ -5,14 +5,25 @@ package shop.util;
 
 import static org.junit.Assert.*;
 
+import java.io.IOException;
+
 import org.junit.Before;
 import org.junit.Test;
 
+import shop.dto.DBTrack;
+
+import de.vdheide.mp3.ID3v2DecompressionException;
+import de.vdheide.mp3.ID3v2IllegalVersionException;
+import de.vdheide.mp3.ID3v2WrongCRCException;
+import de.vdheide.mp3.MP3File;
+import de.vdheide.mp3.NoMP3FrameException;
+
 /**
  * @author Andreas
- *
+ * 
  */
 public class TrackfactoryTest {
+	MP3File file = null;
 
 	/**
 	 * @throws java.lang.Exception
@@ -26,7 +37,28 @@ public class TrackfactoryTest {
 	 */
 	@Test
 	public void testCreateTrack() {
-		//fail("Not yet implemented");
+		try {
+
+			file = new MP3File("WebContent/images/wwm.mp3");
+		} catch (ID3v2WrongCRCException e) {
+			e.printStackTrace();
+		} catch (ID3v2DecompressionException e) {
+			e.printStackTrace();
+		} catch (ID3v2IllegalVersionException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		} catch (NoMP3FrameException e) {
+			e.printStackTrace();
+		}
+		
+		DBTrack underTest = Trackfactory.createTrack(file);
+		assertEquals(underTest.getTrackArtist(), "myArtistName");
+		assertEquals(underTest.getTrackDiskNumber(), 3);
+		assertEquals(underTest.getTrackGenre(), "Rock/Pop");
+		assertEquals(underTest.getTrackNumber(), 1);
+		assertEquals(underTest.getTrackTitle(), "myTrackTitle");
+		assertEquals(underTest.getTrackDate(), 2000);
 	}
 
 }
