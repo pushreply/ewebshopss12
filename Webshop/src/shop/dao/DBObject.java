44652128751cc48fa2e59106eb4e8e6b5738e277
benjamin.oswald1990@gmail.com <benjamin.oswald1990@gmail.com@323ef242-e3e8-b9c7-4c3b-e6e2eb09e930>
2012-04-25 10:31:35 +0000

    DBObject an Client-Server Struktur angepasst

--- Webshop/src/shop/dao/DBObject.java
+++ Webshop/src/shop/dao/DBObject.java
@@ -4,6 +4,15 @@ package shop.dao;
  * @author mukunzi, Benjamin
  */
 
+import shop.dto.DBAddress;
+import shop.dto.DBAlbum;
+import shop.dto.DBCategory;
+import shop.dto.DBCustomer;
+import shop.dto.DBItems;
+import shop.dto.DBKeyword;
+import shop.dto.DBOrder;
+import shop.dto.DBTrack;
+
 import com.db4o.Db4oEmbedded;
 import com.db4o.ObjectContainer;
 import com.db4o.config.EmbeddedConfiguration;
@@ -13,46 +22,53 @@ import com.db4o.ext.DatabaseFileLockedException;
 
 public class DBObject {
 	
-	public  ObjectContainer getConnection(ClientConfiguration config)
+	public  ObjectContainer getConnection()
 	{
-        ObjectContainer db = null;
 		
-        db = Db4oClientServer.openClient(config, "localhost", 4488, "db4o", "db4o");
+		ClientConfiguration config = Db4oClientServer.newClientConfiguration(); //Eine neue Client-Configuration wird angelegt
+		initConfig(config);
+		ObjectContainer client = Db4oClientServer.openClient(config, "localhost", 4488, "db4o", "db4o");
+	
+        System.out.println("getConnection() wurde ausgeführt ausgeführt");
         
-		/*try
-		{
-		
-		if(file ==null)
-		{
-			throw new NullPointerException(file +" darf nicht null sein ");
-		}
-		
-		else if(config == null)
-		{
-			// Öffne Datenbank
-			db = Db4oEmbedded.openFile(file);
-		}
-		else
-		{
-		  // Öffne Datenbank
-		  db = Db4oEmbedded.openFile(config, file);
-		}
-		  
-		}
-		catch(DatabaseFileLockedException  e)
-		{
-			e.printStackTrace();
-		}*/
-		
-        System.out.println("getConnection() wird ausgeführt");
-        
-		return db;
+		return client;
 	}
 	
-	/*
-	public ObjectContainer getConnection(String file)
-	{
-		return getConnection(file, null);
-	}*/
+	
+	private void initConfig(ClientConfiguration config) {
+		// Klasse Address
+		config.common().objectClass(DBAddress.class).cascadeOnUpdate(true);
+		config.common().objectClass(DBAddress.class).cascadeOnDelete(true);
+		
+		// Klasse Album
+		config.common().objectClass(DBAlbum.class).cascadeOnUpdate(true);
+		config.common().objectClass(DBAlbum.class).cascadeOnDelete(true);
+		
+		// Klasse Category
+		config.common().objectClass(DBCategory.class).cascadeOnUpdate(true);
+		config.common().objectClass(DBCategory.class).cascadeOnDelete(true);
+		
+		// Klasse Customer
+		config.common().objectClass(DBCustomer.class).cascadeOnUpdate(true);
+		config.common().objectClass(DBCustomer.class).cascadeOnDelete(true);
+		
+		// Klasse Items
+		config.common().objectClass(DBItems.class).cascadeOnUpdate(true);
+		config.common().objectClass(DBItems.class).cascadeOnDelete(true);
+		
+
+		// Klasse Keyword
+		config.common().objectClass(DBKeyword.class).cascadeOnUpdate(true);
+		config.common().objectClass(DBKeyword.class).cascadeOnDelete(true);
+		
+		// Klasse Order
+		config.common().objectClass(DBOrder.class).cascadeOnUpdate(true);
+		config.common().objectClass(DBOrder.class).cascadeOnDelete(true);
+
+
+		// Klasse Track
+		config.common().objectClass(DBTrack.class).cascadeOnUpdate(true);
+		config.common().objectClass(DBTrack.class).cascadeOnDelete(true);
+	}
 	
 }
