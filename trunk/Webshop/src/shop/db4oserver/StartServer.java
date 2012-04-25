261a3e2f75b56c8d0ef909bb9dae1f91a64ba0db
benjamin.oswald1990@gmail.com <benjamin.oswald1990@gmail.com@323ef242-e3e8-b9c7-4c3b-e6e2eb09e930>
2012-04-25 10:28:57 +0000

    StartServer geändert

--- trunk/Webshop/src/shop/db4oserver/StartServer.java
+++ trunk/Webshop/src/shop/db4oserver/StartServer.java
@@ -4,6 +4,15 @@
 
 package shop.db4oserver;
 
+import shop.dto.DBAddress;
+import shop.dto.DBAlbum;
+import shop.dto.DBCategory;
+import shop.dto.DBCustomer;
+import shop.dto.DBItems;
+import shop.dto.DBKeyword;
+import shop.dto.DBOrder;
+import shop.dto.DBTrack;
+
 import com.db4o.*;
 import com.db4o.constraints.UniqueFieldValueConstraint;
 import com.db4o.cs.*;
@@ -43,41 +52,61 @@ public class StartServer implements ServerInfo, MessageRecipient {
         synchronized (this) {
             ServerConfiguration config = Db4oClientServer.newServerConfiguration();
             
-            /*config.common().objectClass(DBPersonen.class).cascadeOnDelete(true);
-    		config.common().objectClass(DBPersonen.class).cascadeOnUpdate(true);
+            
+            // Klasse Address
+            config.common().objectClass(DBAddress.class).cascadeOnUpdate(true);
+    		config.common().objectClass(DBAddress.class).cascadeOnDelete(true);
+    		
+    		// Klasse Album
+    		config.common().objectClass(DBAlbum.class).cascadeOnUpdate(true);
+    		config.common().objectClass(DBAlbum.class).cascadeOnDelete(true);
+    		
+    		// Klasse Category
+    		config.common().objectClass(DBCategory.class).cascadeOnUpdate(true);
+    		config.common().objectClass(DBCategory.class).cascadeOnDelete(true);
+    		
+    		// Klasse Customer
+    		config.common().objectClass(DBCustomer.class).cascadeOnUpdate(true);
+    		config.common().objectClass(DBCustomer.class).cascadeOnDelete(true);
+    		
+    		// Klasse Items
+    		config.common().objectClass(DBItems.class).cascadeOnUpdate(true);
+    		config.common().objectClass(DBItems.class).cascadeOnDelete(true);
+    		
+
+    		// Klasse Keyword
+    		config.common().objectClass(DBKeyword.class).cascadeOnUpdate(true);
+    		config.common().objectClass(DBKeyword.class).cascadeOnDelete(true);
+    		
+    		// Klasse Order
+    		config.common().objectClass(DBOrder.class).cascadeOnUpdate(true);
+    		config.common().objectClass(DBOrder.class).cascadeOnDelete(true);
+
+
+    		// Klasse Track
+    		config.common().objectClass(DBTrack.class).cascadeOnUpdate(true);
+    		config.common().objectClass(DBTrack.class).cascadeOnDelete(true);
     		
     		config.common().activationDepth(5);
     		
-    		config.common().objectClass(DBPersonen.class).objectField("persnr").indexed(true);
-    		config.common().add(new UniqueFieldValueConstraint(DBPersonen.class, "persnr"));
-    		
-    		config.common().objectClass(DBAdresse.class).objectField("adrnr").indexed(true);
-    		config.common().add(new UniqueFieldValueConstraint(DBAdresse.class, "adrnr"));
-    		
-    		config.common().objectClass(DBAusbildungen.class).objectField("anr").indexed(true);
-    		config.common().add(new UniqueFieldValueConstraint(DBAusbildungen.class, "anr"));
-    		
-    		config.common().objectClass(DBBeruf.class).objectField("bnr").indexed(true);
-    		config.common().add(new UniqueFieldValueConstraint(DBBeruf.class, "bnr"));
-    		
-    		config.common().objectClass(DBHobbies.class).objectField("hnr").indexed(true);
-    		config.common().add(new UniqueFieldValueConstraint(DBHobbies.class, "hnr"));*/
-            
             
             // Using the messaging functionality to redirect all
             // messages to this.processMessage
             config.networking().messageRecipient(this);
             
-            
-            ObjectServer db4oServer = Db4oClientServer.openServer(config
-                    , FILE, PORT);
+            //Öffnen des DB4O-Server
+            ObjectServer db4oServer = Db4oClientServer.openServer(config, FILE, PORT);
             db4oServer.grantAccess(USER, PASS);
+            
+            System.out.println("DB-File: " + FILE + " Port:" +  PORT);
             
             // to identify the thread in a debugger
             Thread.currentThread().setName(this.getClass().getName());
+            
             // We only need low priority since the db4o server has
             // it's own thread.
             Thread.currentThread().setPriority(Thread.MIN_PRIORITY);
+            
             try {
                 if (!stop) {
                     // wait forever for notify() from close()
