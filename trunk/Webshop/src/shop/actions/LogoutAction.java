2ac64dc7b15f5fc848ff1598754f201e4d1c416f
ronaldo.hasiholan@gmail.com <ronaldo.hasiholan@gmail.com@323ef242-e3e8-b9c7-4c3b-e6e2eb09e930>
2012-05-07 13:04:24 +0000

    session works now..really ;)

---
+++ trunk/Webshop/src/shop/actions/LogoutAction.java
@@ -0,0 +1,51 @@
+package shop.actions;
+
+import java.io.IOException;
+import java.util.UUID;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import shop.dao.DAOCustomer;
+import shop.dao.GenericDaoImpl;
+import shop.dao.IGenericDao;
+import shop.dto.DBCustomer;
+
+import com.db4o.ObjectContainer;
+
+/**
+ * This Action handles all needs of categories
+ * 
+ * @author roha0001
+ * 
+ * 
+ */
+public class LogoutAction  extends AbstractAction{
+
+	@Override
+	protected void process(HttpServletRequest request,
+			HttpServletResponse response, ObjectContainer db)
+			throws ServletException, IOException {
+
+		if (request.getParameter("Logout") != null) {
+			RequestDispatcher disp = request.getRequestDispatcher("/login.jsp");
+			try {
+				
+				HttpSession session = request.getSession(false);
+				if (session != null) {
+					session.invalidate();
+					System.out.println("Logging out");
+					disp.forward(request, response);
+				}
+			} catch (Exception e) {
+				errorHandler.toUser("Etwas mit der Weiterleitung ist schief gelaufen.", e);
+			}
+		}
+		
+		
+	}
+
+}
